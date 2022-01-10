7e7159e521704bbc3ed31f88b53f9b14480cf4f7
Peter Song <psong562@gmnail.com>
2022-01-09 19:50:47 -0800

    added .env

--- db/index.js
+++ db/index.js
@@ -1,3 +1,6 @@
 const { Sequelize } = require('sequelize')
 
 module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : 'mysql://root:rootroot@localhost:3306/blog_db')
+
+
+process.env.JAWSDB_URL || process.env.LOCALDB_URL
