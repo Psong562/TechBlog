ec7a8b14fdb7e3314747e18f85a6365c700cafa1
Peter Song <psong562@gmnail.com>
2022-01-09 19:51:14 -0800

    updated db

--- db/index.js
+++ db/index.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : 'mysql://root:rootroot@localhost:3306/blog_db')
+module.exports = new Sequelize(process.env.JAWSDB_URL || process.env.LOCALDB_URL)
 
 
-process.env.JAWSDB_URL || process.env.LOCALDB_URL
