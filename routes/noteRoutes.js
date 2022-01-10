d20ad387ef9e9f0ed9f6c0df09d090330282889b
Peter Song <psong562@gmnail.com>
2022-01-10 01:05:20 -0800

    updated post

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -2,15 +2,19 @@ const router = require('express').Router()
 const { Post, User, Note } = require('../models')
 const passport = require('passport')
 
-// GET all posts
+// GET all comment
 router.get('/notes', passport.authenticate('jwt'), async function (req, res) {
   const notes = await Note.findAll({ include: [User, Post] })
   res.json(notes)
 })
 
 
+router.get('/notes/:id', passport.authenticate('jwt'), async function (req, res) {
+  const note = await Note.findAll({ where: { pid: req.params.id }, include: [User] })
+  res.json(note)
+})
 
-// POST one post
+// POST one comment
 router.post('/notes', passport.authenticate('jwt'), async function (req, res) {
   const note = await Note.create({
     body: req.body.body,
@@ -20,10 +24,10 @@ router.post('/notes', passport.authenticate('jwt'), async function (req, res) {
   res.json(note)
 })
 
-// DELETE one post
-// router.delete('/posts/:id', passport.authenticate('jwt'), async function (req, res) {
-//   await Post.destroy({ where: { id: req.params.id } })
-//   res.sendStatus(200)
-// })
+// DELETE one comment
+router.delete('/notes/:id', passport.authenticate('jwt'), async function (req, res) {
+  await Note.destroy({ where: { id: req.params.id } })
+  res.sendStatus(200)
+})
 
 module.exports = router
